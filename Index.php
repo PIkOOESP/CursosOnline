dd95a5037d866376a8be8fe4f9534f2b3f9378f1
Miguel <miguelangel.s.b86@gmail.com>
2025-04-07 21:12:55 +0200

    Segundo commit

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php
+    require('/Conexion/conexion.php');
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/estilo/estilos.css">
+        <link rel="icon" type="image/x-icon" href="">
+
+    </head>
+
+    <body>
+        
+    </body>
+</html>
