b5ec8deaf971bcb0ec11d423beedec3ae752962c
PIkOOESP <miguelangel.s.b86@gmail.com>
2025-05-01 20:24:43 +0200

    login

--- Conexion/conexion.php
+++ Conexion/conexion.php
@@ -3,6 +3,5 @@
     $password="root";
     $servidor="localhost";
     $basededatos="cursosonline";
-    $conexion =mysqli_connect($usuario,$password,$servidor);
-    $db=mysqli_select_db($conexion,$basededatos);
+    $conexion = new mysqli($servidor,$usuario,$password,$basededatos);
 ?>
