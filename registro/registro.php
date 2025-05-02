4f18357292ca22b4a4b3f37f7311b787f7537f00
pablo <pablito20001@gmail.com>
2025-05-02 17:03:11 +0200

    Actualización en conexión, estilos y registro

--- registro/registro.php
+++ registro/registro.php
@@ -15,52 +15,56 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="estilos/estilos.css">
+        <link rel="stylesheet" href="/CursosOnline/estilo/estilos.css">
         <link rel="icon" type="image/x-icon" href="">
         <script src="registro.js"></script>
         <title>Registro</title>
     </head>
 
     <body>
-        <div class="registro_titulo">
-            <h1>Registrar</h1>
-        </div>    
+        
+           
 
         <div class="registro_cuerpo">
-            <form action="autenticacion.php" method="post" id="formulario">
-                <input type="hidden" name="admin" value="<?php echo $_GET['admin']; ?>">
 
-                <?php  
-                    if($error == 0){ 
-                        echo"<p>Correo ya registrado, pruebe otro o <a href='../login/login.php?admin=".$_GET['admin']."'>inicie sesion</a></p>";
-                    } else if($error == 1){
-                        echo"<p>Error al registrar los datos, pruebe otra vez mas tarde</p>";
-                    }
-                         
-                ?>
+            <h1 class="registro_titulo">Registrar</h1>
+     
+                <form action="autenticacion.php" method="post" id="formulario">
 
-                <label for="nombre">Nombre</label>
-                <input type="text" name="nombre" id="nombre">
+                    <input type="hidden" name="admin" value="<?php echo $_GET['admin']; ?>">
 
-                <p id=error_nombre></p>
+                    <?php  
+                        if($error == 0){ 
+                            echo"<p>Correo ya registrado, pruebe otro o <a href='../login/login.php?admin=".$_GET['admin']."'>inicie sesion</a></p>";
+                        } else if($error == 1){
+                            echo"<p>Error al registrar los datos, pruebe otra vez mas tarde</p>";
+                        }
+                            
+                    ?>
 
-                <label for="apellido">Apellidos</label>
-                <input type="text" name="apellido" id="apellido">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre" id="nombre">
 
-                <p id=error_apellido></p>
+                    <p id=error_nombre></p>
 
-                <label for="correo">Correo electronico</label>
-                <input type="email" name="correo" id="correo">
+                    <label for="apellido">Apellidos</label>
+                    <input type="text" name="apellido" id="apellido">
 
-                <p id=error_correo></p>
+                    <p id=error_apellido></p>
 
-                <label for="password">Contraseña</label>
-                <input type="password" name="password" id="password">
+                    <label for="correo">Correo electronico</label>
+                    <input type="email" name="correo" id="correo">
 
-                <p id=error_password></p>
+                    <p id=error_correo></p>
 
-                <button type="button" onclick="verificar()">Enviar</button>
-            </form>
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password" id="password">
+
+                    <p id=error_password></p>
+
+                    <button type="button" onclick="verificar()">Enviar</button>
+                    
+                </form>
         </div>
     </body>
 </html>
