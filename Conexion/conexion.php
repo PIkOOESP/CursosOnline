da6dcf7173502c0bb23dc81c23f9eeb4a863b72b
pablo <pablito20001@gmail.com>
2025-05-02 17:54:53 +0200

    Actualización en conexión, estilos y registro

--- Conexion/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $usuario="root";
-    $password="root";
-    $servidor="localhost";
-    $basededatos="cursosonline";
-    $conexion =mysqli_connect($usuario,$password,$servidor);
-    $db=mysqli_select_db($conexion,$basededatos);
-?>
