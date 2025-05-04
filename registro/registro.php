9b3722d521685225e0b90c60c67186628cf70d21
PIkOOESP <miguelangel.s.b86@gmail.com>
2025-05-04 11:36:22 +0200

    ruta estilos corregida

--- registro/registro.php
+++ registro/registro.php
@@ -15,7 +15,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/CursosOnline/estilo/estilos.css">
+        <link rel="stylesheet" href="../estilo/estilos.css">
         <link rel="icon" type="image/x-icon" href="">
         <script src="registro.js"></script>
         <title>Registro</title>
